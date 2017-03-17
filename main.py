09572413d3c359c850a486e60b99b7c322a12b2e
Jan Timpe <jantimpe@email.uark.edu>
2017-03-17 12:36:13 -0500

    Implemented tests for all required cases

--- main.py
+++ main.py
@@ -1,33 +1,29 @@
 from models import User
-    
+
 def exchange_keys(sender, recipient):
     sender.create_shared_key()
     key = sender.shared_key
-    # print(key) # debug
-    # key = "this is a message"
     enc_key = sender.rsa_encrypt(key, 32, recipient.public_key())
     recipient.set_shared_key(recipient.rsa_decrypt(enc_key))
-    # print(recipient.shared_key) # debug
 
+# Define Users
 alice = User("Alice")
 bob = User("Bob")
 
+# Perform key exchange
 exchange_keys(alice, bob)
+print("1. "+str(alice.shared_key == bob.shared_key)) # verify that the key exchange was successful
 
 msg = "bite my shiny metal ass"
 
-# ciphertext = alice.write_message(msg)
-# print("MSG: "+str(bob.read_message()))
+# Using AES, write an encrypted message to a file to be read by another user
+ciphertext = alice.write_message(msg)
+print("2. "+str(str(bob.read_message()) == msg))
 
+# Using HMAC, sign a message with a shared key to be verified by another user
+signature = alice.hmac_sign(msg)
+print("3. "+str(bob.hmac_auth(msg, signature)))
 
-# signature = alice.rsa_sign(msg, 16)
-# if bob.rsa_auth(msg, signature, alice.public_key()):
-#     print("passed")
-# else:
-#     print("failed")
-
-# signature = alice.hmac_sign(msg)
-# if bob.hmac_auth(msg, signature):
-#     print("passed")
-# else:
-#     print("failed")
+# Using RSA digital signatures, sign a message to be verified by another user
+signature = alice.rsa_sign(msg, 16)
+print("4. "+str(bob.rsa_auth(msg, signature, alice.public_key())))

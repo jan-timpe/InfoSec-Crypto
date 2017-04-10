50122cc6a7221827838f876ebbdf2d7caee17126
Jan Timpe <jantimpe@email.uark.edu>
2017-04-10 16:02:04 -0500

    Cleanup, comments, wrote testing functions

--- main.py
+++ main.py
@@ -6,24 +6,61 @@ def exchange_keys(sender, recipient):
     enc_key = sender.rsa_encrypt(key, 32, recipient.public_key())
     recipient.set_shared_key(recipient.rsa_decrypt(enc_key))
 
+# Perform key exchange
+def test_key_exchange(sender, recipient):
+    print("1. Testing Key Exchange")
+    exchange_keys(sender, recipient)
+    print("Sender Key: "+str(sender.shared_key))
+    print("Recip. Key: "+str(recipient.shared_key))
+    if sender.shared_key == recipient.shared_key:
+        print("Passed!")
+    else:
+        print("Failed.")
+
+# Using AES, write an encrypted message to a file to be read by another user
+def test_aes_encryption(sender, recipient, message):
+    print("2. Testing AES Encryption")
+    ciphertext = sender.write_message(message)
+    print("Original Message: "+message)
+    decrypted = str(recipient.read_message())
+    print("Decrypt. Message: "+decrypted)
+    if decrypted == message:
+        print("Passed!")
+    else:
+        print("Failed.")
+
+# Using HMAC, sign a message with a shared key to be verified by another user
+def test_hmac_signature(sender, recipient, message):
+    print("3. Testing HMAC Authentication")
+    signature = sender.hmac_sign(message)
+    is_verified = recipient.hmac_auth(message, signature)
+    if is_verified:
+        print("Passed!")
+    else:
+        print("Failed.")
+
+# Using RSA digital signatures, sign a message to be verified by another user
+def test_rsa_signature(sender, recipient, message):
+    print("4. Testing RSA Authentication")
+    signature = sender.rsa_sign(message, 16)
+    is_verified = recipient.rsa_auth(message, signature, sender.public_key())
+    if is_verified:
+        print("Passed!")
+    else:
+        print("Failed.")
+
 # Define Users
 alice = User("Alice")
 bob = User("Bob")
 
-# Perform key exchange
-exchange_keys(alice, bob)
-print("1. "+str(alice.shared_key == bob.shared_key)) # verify that the key exchange was successful
+# perform tests!
+test_key_exchange(alice, bob)
 
-msg = "bite my shiny metal ass"
+msg = "this string is 25 bytes!"
+test_aes_encryption(alice, bob, msg)
 
-# Using AES, write an encrypted message to a file to be read by another user
-ciphertext = alice.write_message(msg)
-print("2. "+str(str(bob.read_message()) == msg))
+msg = "could be a 30 byte long string"
+test_hmac_signature(alice, bob, msg)
 
-# Using HMAC, sign a message with a shared key to be verified by another user
-signature = alice.hmac_sign(msg)
-print("3. "+str(bob.hmac_auth(msg, signature)))
-
-# Using RSA digital signatures, sign a message to be verified by another user
-signature = alice.rsa_sign(msg, 16)
-print("4. "+str(bob.rsa_auth(msg, signature, alice.public_key())))
+msg = "i'll take one 40 byte long string please"
+test_rsa_signature(alice, bob, msg)
--- models.py
+++ models.py
@@ -2,22 +2,26 @@ import ast, base64, hashlib, hmac, random, string
 import Crypto
 from Crypto import Random
 from Crypto.Cipher import AES
-from Crypto.Hash import SHA256 
+from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 
+# generates a cryptographically secure random string
 def generate_secret_key(size):
     secret = Random.new().read(size)
     return secret
-    
+
+# hashes a string using SHA256
 def hash_message(message_txt):
     message_hash = hashlib.sha256(message_txt.encode()).digest()
     return message_hash
-    
+
+# NOTE: writes the passed in string into a binary file
 def write_to_file(txt):
     f = open("messages.txt", "wb")
     f.write(txt)
     f.close()
 
+# NOTE: reads the contents of a binary file
 def read_from_file():
     f = open("messages.txt", "rb")
     msg = f.read()
@@ -28,61 +32,77 @@ class User:
         self.name = name
         random_generator = Random.new().read
         self.key_pair = RSA.generate(1024, random_generator)
-        
+
     def set_shared_key(self, key):
         self.shared_key = key
-        
+
     def create_shared_key(self):
         key = generate_secret_key(AES.block_size)
         self.set_shared_key(key)
-            
+
     def public_key(self):
         return self.key_pair.publickey()
-        
+
+    # returns a message encrypted with a specified public key
+    # NOTE: k exists for legacy reasons and is effectively ignored. pass in an integer.
     def rsa_encrypt(self, msg_txt, k, public_key):
         encrypted_msg = public_key.encrypt(msg_txt, k)
         return encrypted_msg[0]
-    
+
+    # attempts to decrypt an encrypted message using the key pair owned by the user
     def rsa_decrypt(self, encrypted_msg):
         plain_msg = self.key_pair.decrypt(ast.literal_eval(str(encrypted_msg)))
         return plain_msg
-        
+
+    # signs a hash of a plain text message and returns the signature
+    # NOTE: k exists for legacy reasons and is effectively ignored. pass in an integer.
     def rsa_sign(self, message_txt, k):
         message_hash = hash_message(message_txt)
         signed_hash = self.key_pair.sign(message_hash, k)
         return signed_hash
-        
+
+    # attempts to verify the signed message using the public key passed in as an argument
     def rsa_auth(self, message_txt, signed_hash, pub_key):
         message_hash = hash_message(message_txt)
         is_verified = pub_key.verify(message_hash, signed_hash)
         return is_verified
-        
+
+    # returns the message encrypted by the shared key
+    # NOTE: a shared key needs to be generated and set using set_shared_key() before this method can be used
     def aes_encrypt(self, msg_txt):
         init_vector = Random.new().read(AES.block_size)
         cipher = AES.new(self.shared_key, AES.MODE_CFB, init_vector)
         encrypted_msg = init_vector + cipher.encrypt(msg_txt)
         return encrypted_msg
-        
+
+    # attempts to decrypt a ciphertext using the shared key
+    # NOTE: a shared key needs to be generated and set using set_shared_key() before this method can be used
     def aes_decrypt(self, ciphertext):
         init_vector = ciphertext[:AES.block_size]
         encrypted_msg = ciphertext[AES.block_size:]
         cipher = AES.new(self.shared_key, AES.MODE_CFB, init_vector)
         plain_msg = cipher.decrypt(encrypted_msg)
         return plain_msg
-        
+
+    # encrypts a message and writes the result to a binary file using write_to_file (declared at top of this file)
     def write_message(self, msg):
         encrypted_msg = self.aes_encrypt(msg)
         write_to_file(encrypted_msg)
-        
+
+    # attempts to decrypt the message inside of the binary file using read_from_file (declared at top of this file)
     def read_message(self):
         encrypted_msg = read_from_file()
         plain_msg = self.aes_decrypt(encrypted_msg)
         return plain_msg.decode('utf-8')
-        
+
+    # creates and returns a signed hash of a message using the shared key
+    # NOTE: a shared key needs to be generated and set using set_shared_key() before this method can be used
     def hmac_sign(self, message_txt):
         signed_hash = hmac.new(self.shared_key, message_txt.encode(), hashlib.sha256).digest()
         return signed_hash
-        
+
+    # Attempts to verify a signed hash by recreating the signature with the shared key
+    # NOTE: a shared key needs to be generated and set using set_shared_key() before this method can be used
     def hmac_auth(self, message_txt, signed_hash):
         computed_hash = hmac.new(self.shared_key, message_txt.encode(), hashlib.sha256).digest()
         return computed_hash == signed_hash
